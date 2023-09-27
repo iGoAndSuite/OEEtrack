124c852e18cc4358972a343a6b6756eb26371e51
Gerardo Estrada <gerardoestrada@MacBook-Air-de-Gerardo.local>
2023-09-27 11:10:18 -0700

    Se agrego la funcion main() en main.py

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ from uploadData import *
 import time
 from threading import Thread
 
-
-adc_anterior = [False, False, False] # Inicializar una variable para guardar estado anterior del ADC
-preset = get_upload_rate() #obtiene el valor necesario a esperar para subir informacion
-t_act=time.time()
-t_ant=t_act
-while True:
+def main():
+    adc_anterior = [False, False, False] # Inicializar una variable para guardar estado anterior del ADC
+    preset = get_upload_rate() #obtiene el valor necesario a esperar para subir informacion
     t_act=time.time()
-    adc_anterior = controlar_ADC(adc_anterior) #captura datos de las entradas analogas
-    if t_act-t_ant>preset: #verifica que sea momento de subir datos
-        t = Thread(target = upload_and_delete())
-        t.start()
-        t_ant=t_act
+    t_ant=t_act
+    while True:
+        t_act=time.time()
+        adc_anterior = controlar_ADC(adc_anterior) #captura datos de las entradas analogas
+        if t_act-t_ant>preset: #verifica que sea momento de subir datos
+            t = Thread(target = upload_and_delete())
+            t.start()
+            t_ant=t_act
   
-
+if __name__ == "__main__":
+    main()
     
